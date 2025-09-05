eeb36bd18ebcd07771e4ad64fb97b4e109fab9b6
Daniel Ebenezer <jdaniel.ebenezer@gmail.com>
2025-09-05 20:33:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+updateCount = document.getElementById("count")
+// saveCount = document.getElementById("Prev")
+
+
+let val = 0
+function Click() {
+     val+=1
+     updateCount.textContent = val
+
+    
+}
+
+/*function Save() {
+
+    
+    let savedClick = val + " , "
+    saveCount.textContent += savedClick
+    console.log(val)
+    updateCount.textContent = 0
+    val=0
+
+}
+*/
